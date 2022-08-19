1aff34d0490c9a1f31d30f777e90d0cefee545f3
epsj93 <epsj.93@gmail.com>
2022-08-18 21:10:46 -0300

    params

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Iniciando o Mines!</Text>
+        <Text>
+          Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}{' '}
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  
+})
