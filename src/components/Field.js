bb670420fe9510a4fe438d85b7a1d97dd9cbac6e
epsj93 <epsj.93@gmail.com>
2022-08-25 20:09:53 -0300

    mine

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import params from '../params';
+import Mine from './Mine';
 
 export default (props) => {
-  const { mined, opened, nearMines } = props;
+  const { mined, opened, nearMines, exploded } = props;
   const styleField = [styles.field];
   if (opened) styleField.push(styles.opened);
+  if (exploded) styleField.push(styles.exploded);
   if (styleField.length === 1) styleField.push(styles.regular);
   let color = null;
   if (nearMines > 0) {
@@ -21,6 +23,7 @@ export default (props) => {
       ) : (
         false
       )}
+      {mined && opened ? <Mine /> : false}
     </View>
   );
 };
@@ -47,5 +50,9 @@ const styles = StyleSheet.create({
   label: {
     fontWeight: 'bold',
     fontSize: params.fontSize,
+  },
+  exploded: {
+    backgroundColor: 'red',
+    borderColor: 'red',
   },
 });
